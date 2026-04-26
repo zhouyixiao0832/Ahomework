900bb9ed5bc3999827f4a9abd5a90b37f3a56b71
zhouyixiao0832 <zhouyixiao0832@163.com>
2026-04-26 11:20:17 +0800

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,6 @@
 import axios from 'axios';
-export default axios.create({ baseURL: 'http://localhost:5000/api' });
+
+// 生产环境使用 Netlify 域名，本地开发时注释掉下面这行，改用 localhost
+export default axios.create({
+  baseURL: 'https://astounding-jalebi-d6bb33.netlify.app/api'
+});
